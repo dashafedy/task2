2a7ce4a88b59a779cc07ac988a6cb51b6953b759
Dasha Fedy <dashaafedy@gmail.com>
2022-07-13 20:32:29 +0300

    correction

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 9_000;
+        int miles = amount / 20;
+
+        System.out.println(miles + " миль начисленно за купленный билет стоимость " + (amount));
+
+
+    }
+}
